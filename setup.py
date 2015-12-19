e1c8363ce4324f0601e94f48c99e9b6cf5899639
cvolawless <clairevo@gmail.com>
2015-12-18 18:43:11 -0600

    update to 0.3.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='optimizely',
-      version='0.3',
+      version='0.3.1',
       description='An interface to Optimizely\'s REST API.',
       url='https://github.com/optimizely/optimizely-client-python',
       author='Optimizely',
